9e7160f0b114a80907828fbe464cbf2e8896c224
Mintaka <spaceisperfect@gmail.com>
2019-01-28 17:28:44 +0300

    Merge step10. Added Delete and Restore buttons to hive list and hive section list. Added onDelete and onRestore implementations.

--- KatlaSportNg/src/app/hive-management/lists/hive-section-list.component.ts
+++ KatlaSportNg/src/app/hive-management/lists/hive-section-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HiveSectionListItem } from '../models/hive-section-list-item';
 import { HiveService } from '../services/hive.service';
+import { HiveSectionService } from '../services/hive-section.service';
 
 @Component({
   selector: 'app-hive-section-list',
@@ -16,7 +17,8 @@ export class HiveSectionListComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private hiveService: HiveService
+    private hiveService: HiveService,
+    private hiveSectionService: HiveSectionService
   ) { }
 
   ngOnInit() {
@@ -24,5 +26,15 @@ export class HiveSectionListComponent implements OnInit {
       this.hiveId = p['id'];
       this.hiveService.getHiveSections(this.hiveId).subscribe(s => this.hiveSections = s);
     })
+  }
+
+  onDelete(sectionId : number){
+    var section = this.hiveSections.find(i => i.id == sectionId);
+    this.hiveSectionService.setHiveSectionStatus(sectionId, true).subscribe(i => section.isDeleted = true);
+  }
+
+  onUndelete(sectionId : number){
+    var section = this.hiveSections.find(i => i.id == sectionId);
+    this.hiveSectionService.setHiveSectionStatus(sectionId, false).subscribe(i => section.isDeleted = false);
   }
 }
