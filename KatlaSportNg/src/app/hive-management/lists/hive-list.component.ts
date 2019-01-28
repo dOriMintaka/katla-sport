6870e42c7b1a817f1500894dd58d80560e40d964
Mintaka <spaceisperfect@gmail.com>
2019-01-28 17:27:29 +0300

    Added Delete and Restore buttons to hive list and hive section list. Added onDelete and onRestore implementations.

--- KatlaSportNg/src/app/hive-management/lists/hive-list.component.ts
+++ KatlaSportNg/src/app/hive-management/lists/hive-list.component.ts
@@ -27,5 +27,7 @@ export class HiveListComponent implements OnInit {
   }
 
   onRestore(hiveId: number) {
+    var hive = this.hives.find(i => i.id == hiveId);
+    this.hiveService.setHiveStatus(hiveId, false).subscribe(i => hive.isDeleted = false);
   }
 }
